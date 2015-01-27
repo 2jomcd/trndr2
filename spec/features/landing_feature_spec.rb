817ff3176d04c6dc62e93f7221321d3883fe3fb8
Joe McDonough <2jomcd@gmail.com>
2015-01-27 15:49:18 -0800

    Fix specs

--- spec/features/landing_feature_spec.rb
+++ spec/features/landing_feature_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-	feature 'Search what is trending', type: :feature do
-		scenario 'displaying the top 10 by country' do
-			visit '/'
-			within 'h1' do
-			expect(page).to have_content 'Trender home page'
-		end
+feature 'Search what is trending', type: :feature do
+	scenario 'displaying the top 10 by country' do
+		visit '/'
+		expect(page).to have_content 'Trendr'
 	end
 end
