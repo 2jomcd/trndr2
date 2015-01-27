95e52c3e18ddd0f717bd45d69cecc4fd210fc04a
Joe McDonough <2jomcd@gmail.com>
2015-01-27 14:54:32 -0800

    rspec user model base tests

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,23 +1,23 @@
 class Users::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # protected
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Attributes' do
+		it { is_expected.to respond_to :email}
+		it { is_expected.to respond_to :password}
+	end
+
+  describe 'Database' do
+  	it { is_expected.to have_db_column :email}
+  	it { is_expected.to have_db_column :password}
+  end
+
+  describe 'Validations' do
+  	it { is_expected.to validate_presence_of :email }
+  	it { is_expected.to validate_presence_of :password}
+  end
 end
